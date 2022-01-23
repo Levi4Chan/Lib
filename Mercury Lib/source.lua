96dde29b679c2758fe1bdf1ffd3b1c4d44beda87
Jaaaaay2 <68676706+Jaaaaay2@users.noreply.github.com>
2022-01-23 12:39:46 +0800

    Update source.lua

--- Mercury Lib/source.lua
+++ Mercury Lib/source.lua
@@ -54,14 +54,14 @@ local Library = {
 			StrongText = Color3.fromHSV(0, 0, 1),		
 			WeakText = Color3.fromHSV(0, 0, 172/255)
 		},
-		Rust = {
+		--[[Rust = {
 			Main = Color3.fromRGB(37, 35, 33),
 			Secondary = Color3.fromRGB(65, 63, 63),
 			Tertiary = Color3.fromRGB(237, 94, 38),
 
 			StrongText = Color3.fromHSV(0, 0, 1),		
 			WeakText = Color3.fromHSV(0, 0, 172/255)
-		},
+		},]]
 		Aqua = {
 			Main = Color3.fromRGB(19, 21, 21),
 			Secondary = Color3.fromRGB(65, 63, 63),
@@ -74,7 +74,7 @@ local Library = {
 		OperaGX = {
 			Main = Color3.fromRGB(35, 30, 30),
 			Secondary = Color3.fromRGB(55, 50, 50),
-			Tertiary = Color3.fromRGB(180, 130, 70),
+			Tertiary = Color3.fromRGB(193, 6, 6),
 
 			StrongText = Color3.fromHSV(0, 0, 1),		
 			WeakText = Color3.fromHSV(0, 0, 172/255)
