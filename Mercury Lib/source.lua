2a8db27092fe70d9189b50e25c14f9ee453a23b7
Jaaaaay2 <68676706+Jaaaaay2@users.noreply.github.com>
2022-01-23 12:52:52 +0800

    Update source.lua

--- Mercury Lib/source.lua
+++ Mercury Lib/source.lua
@@ -72,7 +72,7 @@ local Library = {
 		},
 		Vaporwave = {},
 		OperaGX = {
-			Main = Color3.fromRGB(26, 21, 21),
+			Main = Color3.fromRGB(0, 0, 0),
 			Secondary = Color3.fromRGB(46, 41, 41),
 			Tertiary = Color3.fromRGB(193, 6, 6),
 
