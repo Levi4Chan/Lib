6fed5fb0ab6520f69367376af989ee36e38893d1
Jaaaaay2 <68676706+Jaaaaay2@users.noreply.github.com>
2022-02-12 14:41:14 +0800

    Update source.lua

--- Mercury Lib/source.lua
+++ Mercury Lib/source.lua
@@ -2801,7 +2801,7 @@ function Library:credit(options)
 				Size = UDim2.fromOffset(24, 24),
 				Position = UDim2.new(1, -77, 1, -8),
 				Theme = {BackgroundColor3 = {"Main", 10}}
-			}):round(5):tooltip("copy v3rm")
+			}):round(5):tooltip("Copy Channel Link")
 			local youtube = youtubeContainer:object("ImageLabel", {
 				Image = "http://www.roblox.com/asset/?id=8607142923",
 				Size = UDim2.new(1, -4, 1, -4),
